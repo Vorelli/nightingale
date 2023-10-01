782f7fd643e8d10e30a7eac6fae9add6daed3d28
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-10-01 12:36:48 -0600

    Fix object deep equal and update music metadata

--- src/client/helpers/objectsDeepEqual.ts
+++ src/client/helpers/objectsDeepEqual.ts
@@ -1,29 +1,32 @@
 function metadataToObj(a: MediaMetadata) {
-    return {
-        title: a.title,
-        artist: a.artist,
-        artwork: a.artwork.slice(),
-        album: a.album
-    };
+  return {
+    title: a.title,
+    artist: a.artist,
+    artwork: a.artwork.slice(),
+    album: a.album
+  };
 }
 
 export function objectsDeepEqual(a: any, b: any) {
-    if (typeof a !== typeof b) return false;
+  if (typeof a !== typeof b) return false;
 
-    if (a.artist && a.title && a.artwork && a.album) {
-        a = metadataToObj(a);
-        b = metadataToObj(b);
-    }
+  if (a.artist !== undefined && a.title !== undefined && a.artwork !== undefined && a.album !== undefined) {
+    a = metadataToObj(a);
+    b = metadataToObj(b);
+  }
 
-    const isArr = a instanceof Array;
-    for (var key of !isArr ? Object.keys(a) : a) {
-        let compA = a[key];
-        let compB = b[key];
-        if (compA instanceof Array || typeof compA === "object") {
-            if (!objectsDeepEqual(compA, compB)) return false;
-        } else {
-            if (compA !== compB) return false;
-        }
+  const isArr = a.constructor && a.constructor === Array;
+  const keySource = !isArr ? Object.keys(a) : a;
+  for (let i = 0; i < keySource.length; i++) {
+    let key = keySource[i];
+    let compA = isArr ? a[i] : a[key];
+    let compB = isArr ? b[i] : b[key];
+    if ((compA && compA.constructor === Array) || typeof compA === "object") {
+      //console.log(compA.constructor === Array ? "COMPA is an array" : "COMPA is an object", JSON.stringify(compA), JSON.stringify(compB))
+      if (!objectsDeepEqual(compA, compB)) return false;
+    } else {
+      if (compA !== compB) return false;
     }
-    return true;
+  }
+  return true;
 }
